b902c06222523c725f390e0be7aee309551c6fa1
Aryan <aryanabed48@gmail.com>
2025-05-01 21:53:24 -0400

    Added fuzzy search

--- data/clean_data.py
+++ data/clean_data.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv('Dinesafe.csv')
 df.drop_duplicates(inplace=True)
-df.to_csv('name_to_id.csv', index=False)
+df.to_json('../netlify/functions/name_to_id.json', orient='records', indent=2, index=False)
